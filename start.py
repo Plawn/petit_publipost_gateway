9184bd08b0470d5ab5694ddb6de793e6244a5a6e
Paul Leveau <pleveau@juniorisep.com>
2019-11-05 16:58:13 +0100

    serving with gevent

--- start.py
+++ start.py
@@ -1,3 +1,8 @@
 from app import app
+from gevent.pywsgi import WSGIServer
 
-app.run(host='0.0.0.0', port=5000)
+
+http_server = WSGIServer(('', 5000), app)
+http_server.serve_forever()
+
+# app.run(host='0.0.0.0', port=5000)
