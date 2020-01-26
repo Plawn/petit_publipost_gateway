e9b3129ba6b53c172d719836cec5ac9cfe4ea916
Plawn <plawn.yay@gmail.com>
2020-01-26 17:28:44 +0100

    fix git excel adress

--- build_modules.sh
+++ build_modules.sh
@@ -2,7 +2,7 @@ rm -rf modules > /dev/null
 mkdir modules
 cd  modules
 # git pull the excel_env
-git clone "https://git.dev.juniorisep.com/phoenix/excel-publiposting.git" || { echo "failed to clone" ; exit 2; }
+git clone "https://git.dev.juniorisep.com/phoenix/publiposting-services/excel-publiposting.git" || { echo "failed to clone" ; exit 2; }
 cd excel-publiposting
 yarn install || { echo "failed to install" ; exit 2; }
 yarn build || { echo "failed to build" ; exit 2; }
