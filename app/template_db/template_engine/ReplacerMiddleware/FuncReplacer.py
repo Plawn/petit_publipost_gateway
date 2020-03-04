0b29a0f66d3b3142832e5c49841cf017b66fd0af
Plawn <plawn.yay@gmail.com>
2020-03-04 14:27:04 +0100

    update parser to add context support

--- app/template_db/template_engine/ReplacerMiddleware/FuncReplacer.py
+++ app/template_db/template_engine/ReplacerMiddleware/FuncReplacer.py
@@ -1,6 +1,5 @@
 from typing import Tuple
 from .BaseReplacer import BaseReplacer, ReplacerData
-# from ..better_publiposting.model_handler import INFO_FIELD_NAME
 
 TYPE_SYMBOL = 'type'
 
@@ -13,14 +12,23 @@ class FuncReplacer(BaseReplacer):
 
     @staticmethod
     def from_doc(text: str) -> Tuple[str, dict]:
+        """
+        will transform __DDE__ -> ("DDE")
+        will transoform mission.getStudentDoc__#student,REM__ -> mission.getStudentDoc(#student,"REM")
+        """
         if to_replace_begin.doc_side in text:
-            return (text
-                    .replace(to_replace_begin.doc_side, to_replace_begin.other_side, 1)
-                    .replace(to_replace_end.doc_side, to_replace_end.other_side, 1)
-                    .replace(to_replace_sep.doc_side, to_replace_sep.other_side)), {TYPE_SYMBOL: {'function': {'args': ['String']}}}
-            # String for now as we only have one argument for one function
-            # -> should make it better
-        return text, {}
+            i = text.index(to_replace_begin.doc_side)
+            params_string = text[i+2:-2]
+            params = []
+            for p in params_string.split(','):
+                if p.startswith('#'):
+                    params.append(p)
+                else:
+                    params.append(f'"{p}"')
+            
+            e = text[:i] + f'({",".join(params)})'
+            return (e, {})
+        return (text, {})
 
     @staticmethod
     def to_doc(text: str) -> str:
