e93bf8ab571b34103c39d5aa98cc1d2d723d6c51
Paul Leveau <plawn.yay@gmail.com>
2021-06-09 14:31:34 +0200

    fix connector

--- app/template_db/template_engine/connectors/html_publiposting/html_template.py
+++ app/template_db/template_engine/connectors/html_publiposting/html_template.py
@@ -1,8 +1,42 @@
-from ..docx_publiposting import DocxTemplator
+from dataclasses import dataclass
+from typing import List, Optional
 
-class HtmlTemplator(DocxTemplator):
+from ....minio_creds import PullInformations
+from ...base_template_engine import TemplateEngine
+from ...model_handler import Model, SyntaxtKit
+from ...ReplacerMiddleware import MultiReplacer
+
+SYNTAX_KIT = SyntaxtKit('{{', '}}')
+
+
+@dataclass
+class Settings:
+    host: str
+    secure: bool
+
+
+def add_infos(_dict: dict) -> None:
+    """Will add infos to the field on the fly
+    """
+    _dict.update({'traduction': ''})
+
+
+class HtmlTemplator(TemplateEngine):
     """
     """
     requires_env = []
     supported_extensions = {'html'}
 
+    def __init__(self, filename: str, pull_infos: PullInformations, replacer: MultiReplacer):
+        HtmlTemplator.registered_templates.append(self)
+        super().__init__(filename, pull_infos, replacer)
+
+    def _load_fields(self, fields: Optional[List[str]] = None) -> None:
+        if fields is None:
+            fields: List[str] = self._get_placeholders()
+        cleaned = []
+        for field in fields:
+            field, additional_infos = self.replacer.from_doc(field)
+            add_infos(additional_infos)
+            cleaned.append((field.strip(), additional_infos))
+        self.model = Model(cleaned, self.replacer, SYNTAX_KIT)
